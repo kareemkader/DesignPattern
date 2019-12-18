7e86fa046f0ef9e60a98ec14d83cfca6a289b712
kareem salama <kareemkaz1994@gmial.com>
2019-12-18 20:21:58 +0100

    add data center to make it more flixable

--- src/Chapter2/DataCenters.java
+++ src/Chapter2/DataCenters.java
@@ -1,28 +1,31 @@
 package Chapter2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DataCenters {
 	
-	
-	private float temp;
-	private float humidity;
-	private float pressure;
+	private Map<String, Float> properties;
+//	private float temp;
+//	private float humidity;
+//	private float pressure;
 	private List<Observer> observers;
 	
 	
-	public void setTemp(float temp) {
-		this.temp = temp;
-	}
-
-	public void setHumidity(float humidity) {
-		this.humidity = humidity;
-	}
-
-	public void setPressure(float pressure) {
-		this.pressure = pressure;
-	}
+	
+//	public void setTemp(float temp) {
+//		this.temp = temp;
+//	}
+//
+//	public void setHumidity(float humidity) {
+//		this.humidity = humidity;
+//	}
+//
+//	public void setPressure(float pressure) {
+//		this.pressure = pressure;
+//	}
 
 	public void setObservers(List<Observer> observers) {
 		this.observers = observers;
@@ -31,21 +34,22 @@ public class DataCenters {
 	public DataCenters() {
 		// TODO Auto-generated constructor stub
 		this.observers=new ArrayList<Observer>();
+		this.properties=new HashMap<>();
 	}
 	
 	public List<Observer> getObservers() {
 		return observers;
 	}
 
-	public float getTemp() {
-		return temp;
-	}
-
-	public float getHumidity() {
-		return humidity;
-	}
-
-	public float getPressure() {
-		return pressure;
-	}
+//	public float getTemp() {
+//		return temp;
+//	}
+//
+//	public float getHumidity() {
+//		return humidity;
+//	}
+//
+//	public float getPressure() {
+//		return pressure;
+//	}
 }
