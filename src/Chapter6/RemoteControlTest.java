e1882438bda3d7839d3be80b25be48b3517253d8
kareem salama <kareemkaz1994@gmial.com>
2019-12-27 20:38:58 +0100

    add another Command Object to test our System flexability when we will add another vendor classes

--- src/Chapter6/RemoteControlTest.java
+++ src/Chapter6/RemoteControlTest.java
@@ -1,12 +1,33 @@
 package Chapter6;
 
+import java.io.*;
+
 public class RemoteControlTest{
 	public static void main(String[] args) {
 	SimpleRemoteControl control=new SimpleRemoteControl();
 	Light light=new Light();
 	LightOnCommand lightOn=new LightOnCommand(light);
-	control.setCommand(lightOn);
+	LightOffCommand lightOff=new LightOffCommand(light);
+	BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("choose on or off");
+	try{
+		String order=reader.readLine();
+		switch(order){
+		case "on":
+		control.setCommand(lightOn);
+		control.buttonWasPressed();
+		break;
 
-	control.buttonWasPressed();
+		case "off":
+		control.setCommand(lightOff);
+		control.buttonWasPressed();
+		break;
+	}
+	}
+	catch(Exception e){
+		e.printStackTrace();
+	}
+	
+	
 }
 }
