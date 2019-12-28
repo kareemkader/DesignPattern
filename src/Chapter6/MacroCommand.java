9369caf9367fe27b19f05008fb3d9ef47d86d555
kareem salama <kareemkaz1994@gmial.com>
2019-12-28 20:32:42 +0100

    add macro Command to our system which help us to invoke multi Commands in the same time by packaging thim into single MacroCommand object

---
+++ src/Chapter6/MacroCommand.java
@@ -0,0 +1,18 @@
+package Chapter6;
+public class MacroCommand implements Command{
+
+	Command[] commands;
+
+	public MacroCommand(Command[] commands){
+		this.commands=commands;
+	}
+
+	public void execute(){
+		for(int i=0;i<commands.length;i++){
+			commands[i].execute();
+		}
+	}
+
+	public void undo(){}
+
+}
--- src/Chapter6/RemoteLoader.java
+++ src/Chapter6/RemoteLoader.java
@@ -15,13 +15,28 @@ public class RemoteLoader{
 		Command lightOn=new LightOnCommand(light);
 		Command stereoOn=new StereoOnCommand(stereo);
 
+
+
 		Command fanHigh=new CeilingFanHighCommand(fan);
 		Command fanLow=new CeilingFanLowCommand(fan);
-		
+
+
 		//creating commands for devieces [off commands]
 		Command doorOff=new GarageDoorDownCommand(door);
 		Command lightOff=new LightOffCommand(light);
 		Command stereoOff=new StereoOffCommand(stereo);
+
+
+
+		//creating List of commands to start the party
+		Command[] partyOnList={doorOn,lightOn,stereoOn,fanHigh};
+		Command[] partyOffList={doorOff,lightOff,stereoOff,fanLow};
+
+		//creating commands for Party On
+		Command partyOn=new MacroCommand(partyOnList);
+		//creating commands for party off 
+		Command partyOff=new MacroCommand(partyOffList);
+
 
 		//now let's attches our commands to remote control
 		//[Loading Command objects]
@@ -30,7 +45,7 @@ public class RemoteLoader{
 		remote.setCommand(2,stereoOn,stereoOff);
 			//test undo on The Ceiling Fan
 		remote.setCommand(3,fanHigh,fanLow);
-
+		remote.setCommand(4,partyOn,partyOff);
 		//now we set all configuration right let's start to try to play with some button pushes some of them 
 		System.out.println(remote);
 		remote.onButtonWasPushed(0);
@@ -51,6 +66,10 @@ public class RemoteLoader{
 		remote.offButtonWasPushed(3);
 		remote.undoing();
 		
+		System.out.println("\n \n \n the party Starts now ");
+		remote.onButtonWasPushed(4);
+		System.out.println("\n \n the End of the party");
+		remote.offButtonWasPushed(4);
 
 
 	}
