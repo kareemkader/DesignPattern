e7893ef0877016a14916c1036ce1f9bf689852d9
kareem salama <kareemkaz1994@gmial.com>
2019-12-30 18:30:18 +0100

    this the end of Object Adapter pattern

---
+++ src/Chapter7/DuckAdapter.java
@@ -0,0 +1,16 @@
+package Chapter7;
+
+public class DuckAdapter implements Turkey{
+	Duck duck;
+	public DuckAdapter(Duck duck){
+		this.duck=duck;
+	}
+
+	public void gobble(){
+		duck.quack();
+	}
+
+	public void fly(){
+		duck.fly();
+	}
+}
