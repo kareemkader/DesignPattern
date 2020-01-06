eaeb7014c02b3ffffd4eb2884c7d3cb8834f49df
kareem salama <kareemkaz1994@gmial.com>
2020-01-06 16:17:00 +0100

    Revert "Merge branch 'try_another_approch'"
    This reverts commit b56de9bc55396827f03d740bd46207158a70e35c, reversing
    changes made to 66db4180a6d25ba9e38a427b276703d6ded85011.

--- src/Chapter9/MenuTestDrive.java
+++ src/Chapter9/MenuTestDrive.java
@@ -5,9 +5,8 @@ public class MenuTestDrive{
 		MenuComponent dinerMenu=new Menu("DINER MENU","Lunch");
 		MenuComponent breakFast=new Menu("Break fast","for break fast");
 		// create Menu Item now
-		MenuItemComponent steak=new MenuItem("meat","meat steak",2,false);
-		MenuItemComponent pate=new MenuItem("sweet bread","eat it with cafe",5,true);
-
+		MenuComponent steak=new MenuItem("meat","meat steak",2,false);
+		MenuComponent pate=new MenuItem("sweet bread","eat it with cafe",5,true);
 		
 		dinerMenu.add(steak);
 		breakFast.add(pate);
