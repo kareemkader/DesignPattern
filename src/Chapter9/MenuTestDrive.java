dcbf0741cbf294cd2c9a42ae2cb9e0532aa5a819
kareem salama <kareemkaz1994@gmial.com>
2020-01-06 16:03:11 +0100

    my assumption is working

--- src/Chapter9/MenuTestDrive.java
+++ src/Chapter9/MenuTestDrive.java
@@ -1,12 +1,12 @@
 package Chapter9;
 public class MenuTestDrive{
 	public static void main(String[] args) {
-		Component headMenu=new Menu("All menus","All menus Combined");
-		Component dinerMenu=new Menu("DINER MENU","Lunch");
-		Component breakFast=new Menu("Break fast","for break fast");
+		MenuComponent headMenu=new Menu("All menus","All menus Combined");
+		MenuComponent dinerMenu=new Menu("DINER MENU","Lunch");
+		MenuComponent breakFast=new Menu("Break fast","for break fast");
 		// create Menu Item now
-		Component steak=new MenuItem("meat","meat steak",2,false);
-		Component pate=new MenuItem("sweet bread","eat it with cafe",5,true);
+		MenuItemComponent steak=new MenuItem("meat","meat steak",2,false);
+		MenuItemComponent pate=new MenuItem("sweet bread","eat it with cafe",5,true);
 		
 		dinerMenu.add(steak);
 		breakFast.add(pate);
